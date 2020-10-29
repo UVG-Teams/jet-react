0aefe8b465f898daf90f1dd96802e3be98053fd2
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-10-28 22:17:20 -0600

    Basic structure

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.css';
+
+// import Login from '../Login';
+
+
+const App = () => (
+    <div className = "App">
+        <div>
+            {/* <Manager/> */}
+            Test
+        </div>
+    </div>
+);
+
+export default App;
